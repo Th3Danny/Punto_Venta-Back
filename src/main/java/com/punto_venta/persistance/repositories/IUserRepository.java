e9e085b5a0d7d2ec4a9391908e14e6a7783301d9
Danny <223181@ids.upchiapas.edu.mx>
2026-01-25 21:33:13 -0600

    created crud products

--- src/main/java/com/punto_venta/persistance/repositories/IUserRepository.java
+++ src/main/java/com/punto_venta/persistance/repositories/IUserRepository.java
@@ -2,6 +2,8 @@ package com.punto_venta.persistance.repositories;
 
 import com.punto_venta.persistance.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +12,7 @@ import java.util.Optional;
 public interface IUserRepository extends JpaRepository<Users, Long> {
     @Override
     Optional<Users> findById(Long aLong);
+
     Optional<Users> findByEmail(String email);
 
     Optional<Users> findByUsername(String username);
@@ -17,6 +20,13 @@ public interface IUserRepository extends JpaRepository<Users, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String username);
+
+
+    @Query("select u from Users u left join fetch u.roles where u.email = :email")
+    Optional<Users> findByEmailFetchRoles(@Param("email") String email);
+
+    @Query(value = "SELECT r.role_id FROM user_role r JOIN user u ON u.id = r.user_id WHERE u.email = :email", nativeQuery = true)
+    java.util.List<Long> findRoleIdsByUserEmail(@Param("email") String email);
 }
 
 
